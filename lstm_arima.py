4fb601a4eda60f3ffe83ab0030120de9c5bde358
mnaufalrifqi <mnaufalrifqi10@gmail.com>
2025-02-09 04:08:33 +0700

    lstm_arima.py

--- lstm_arima.py
+++ lstm_arima.py
@@ -311,7 +311,7 @@ elif model_choice == 'ARIMA':
     })
 
      # Adding a column for price change (up or down)
-    predicted_prices['Price Change'] = predicted_prices['Predicted Price'].diff().apply(lambda x: 'naik' if x > 0 else 'turun')
+    predicted_prices['Perubahan Harga'] = predicted_prices['Harga Prediksi'].diff().apply(lambda x: 'naik' if x > 0 else 'turun')
 
     # Displaying the table with the new column
     st.write(predicted_prices)
